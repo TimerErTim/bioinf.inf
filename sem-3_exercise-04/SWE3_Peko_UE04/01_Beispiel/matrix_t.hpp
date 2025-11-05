55cbbf3ea652f5149d78e10a0458aff1a95f6b77
TimerErTim <timerertim@gmail.com>
2025-11-05 15:03:28 +0100

    feat(sem3-ue4): implement without ops matrix and rational

--- sem-3_exercise-04/SWE3_Peko_UE04/01_Beispiel/matrix_t.hpp
+++ sem-3_exercise-04/SWE3_Peko_UE04/01_Beispiel/matrix_t.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#include <iosfwd>
+// Lightweight 1×1 matrix wrapper used to demonstrate rationals over non-int domains.
+// All operations reduce to operations on the single stored scalar value.
+
+#include <ostream>
 
 template <typename T>
 class matrix_t {
@@ -25,7 +28,7 @@ public:
 	friend bool operator!=(matrix_t const& a, matrix_t const& b) { return !(a == b); }
 	friend bool operator<(matrix_t const& a, matrix_t const& b) { return a.a11_ < b.a11_; }
 
-	friend std::ostream& operator<<(std::ostream& os, matrix_t const& m) {
+friend std::ostream& operator<<(std::ostream& os, matrix_t const& m) {
 		os << "[" << m.a11_ << "]";
 		return os;
 	}
@@ -33,46 +36,3 @@ public:
 private:
 	T a11_;
 };
-
-// ops specializations for matrix_t live here to avoid circular includes
-#include "operations.h"
-
-namespace ops {
-
-template <typename T>
-inline ::matrix_t<T> abs(::matrix_t<T> const& a) { return ::matrix_t<T>{ a.value() < T{0} ? -a.value() : a.value() }; }
-
-template <typename T>
-inline bool equals(::matrix_t<T> const& a, ::matrix_t<T> const& b) { return a == b; }
-
-template <typename T>
-inline bool is_zero(::matrix_t<T> const& a) { return a == ::matrix_t<T>::zero(); }
-
-template <typename T>
-inline bool is_negative(::matrix_t<T> const& a) { return a.value() < T{0}; }
-
-template <typename T>
-inline ::matrix_t<T> negate(::matrix_t<T> const& a) { return ::matrix_t<T>{ -a.value() }; }
-
-template <typename T>
-inline bool divides(::matrix_t<T> const& a, ::matrix_t<T> const& b) {
-	// In a 1x1 matrix ring over a field-like T, a divides b iff a != 0
-	return !is_zero(a);
-}
-
-template <typename T>
-inline ::matrix_t<T> remainder(::matrix_t<T> const& /*a*/, ::matrix_t<T> const& /*b*/) {
-	// Euclidean remainder is always zero for our 1x1 field-like matrices when using gcd fallback
-	return ::matrix_t<T>::zero();
-}
-
-template <typename T>
-inline ::matrix_t<T> gcd(::matrix_t<T> a, ::matrix_t<T> b) {
-	// For 1x1 matrices over a field-like T, any non-zero element is a unit; normalize to 1
-	(void)a; (void)b;
-	return ::matrix_t<T>::one();
-}
-
-} // namespace ops
-
-
--- sem-3_exercise-04/SWE3_Peko_UE04/01_Beispiel/rational_t.hpp
+++ sem-3_exercise-04/SWE3_Peko_UE04/01_Beispiel/rational_t.hpp
@@ -1,181 +1,245 @@
 #pragma once
 
-#include <stdexcept>
-#include <string>
-#include <iosfwd>
+#include <concepts>
+#include <istream>
+#include <ostream>
 #include <sstream>
+#include <string>
 #include <type_traits>
 #include <utility>
 
-// forward declare matrix_t for trait
-template <typename> class matrix_t;
-
 #include "errors.hpp"
 
-// generic ops used by rational_t
-#include "operations.h"
-
-// helper trait to detect matrix_t specializations
-template <typename X> struct is_matrix : std::false_type {};
-template <typename U> struct is_matrix<matrix_t<U>> : std::true_type {};
-
 template <typename T>
+// Attempting recursive reference
+concept RationalElement =
+    std::regular<T> &&         // copyable, assignable, equality comparable
+    std::totally_ordered<T> && // <, <=, >, >=, ==, !=
+    std::constructible_from<T, int> && // must be constructible from int
+                                       // literals like 0, 1
+    requires(T a, T b, std::ostream os, std::istream is) {
+      // arithmetic
+      { a + b } -> std::convertible_to<T>;
+      { a - b } -> std::convertible_to<T>;
+      { a * b } -> std::convertible_to<T>;
+      { a / b } -> std::convertible_to<T>;
+      { a % b } -> std::convertible_to<T>;
+
+      // unary minus
+      { -a } -> std::convertible_to<T>;
+
+      // stream I/O
+      { os << a } -> std::same_as<std::ostream&>;
+      { is >> a } -> std::same_as<std::istream&>;
+    };
+
+template <RationalElement T>
 class rational_t {
 public:
-	using value_type = T;
+  using value_type = T;
 
-	rational_t() noexcept : numerator_(T{0}), denominator_(T{1}) {}
-	explicit rational_t(const value_type& numerator) noexcept : numerator_(numerator), denominator_(T{1}) {}
-	rational_t(const value_type& numerator, const value_type& denominator)
-		: numerator_(numerator), denominator_(denominator) {
-		if (ops::is_zero(denominator_)) {
-			throw invalid_rational_error("denominator must not be zero");
-		}
-		normalize();
-	}
-	rational_t(const rational_t&) noexcept = default;
-	rational_t& operator=(const rational_t& other) noexcept {
-		if (this == &other) return *this;
-		numerator_ = other.numerator_;
-		denominator_ = other.denominator_;
-		return *this;
-	}
+  rational_t() noexcept : numerator_(T{0}), denominator_(T{1}) {}
+  rational_t(const value_type &numerator) noexcept : numerator_(numerator), denominator_(T{1}) {}
+  rational_t(const value_type &numerator, const value_type &denominator) : numerator_(numerator), denominator_(denominator) {
+    if (denominator == T{0}) {
+      throw invalid_rational_error("denominator must not be zero");
+    }
+    normalize();
+  }
+  // Default implementation of rule of five is enough
 
-	value_type const& get_numerator() const noexcept { return numerator_; }
-	value_type const& get_denominator() const noexcept { return denominator_; }
+  value_type const &get_numerator() const noexcept { return numerator_; }
+  value_type const &get_denominator() const noexcept { return denominator_; }
 
-	bool is_negative() const noexcept { return ops::is_negative(numerator_); }
-	bool is_positive() const noexcept { return !ops::is_zero(numerator_) && !ops::is_negative(numerator_); }
-	bool is_zero() const noexcept { return ops::is_zero(numerator_); }
+  bool is_negative() const noexcept { return numerator_ < T{0}; }
+  bool is_positive() const noexcept { return numerator_ > T{0}; }
+  bool is_zero() const noexcept { return numerator_ == T{0}; }
 
-	void inverse() {
-		if (ops::is_zero(numerator_)) {
-			throw division_by_zero_error("cannot invert zero");
-		}
-		std::swap(numerator_, denominator_);
-		normalize();
-	}
+  // Replace this rational with its multiplicative inverse (swap
+  // numerator/denominator)
+  void inverse() {
+    if (is_zero()) {  // numerator is zero [would -> denominator is zero!]
+      throw division_by_zero_error("cannot invert zero");
+    }
+    std::swap(numerator_, denominator_);
+    normalize();
+  }
 
-	std::string as_string() const {
-		std::ostringstream out;
-		if (ops::equals(denominator_, T{1})) {
-			out << numerator_;
-		} else {
-			out << numerator_ << '/' << denominator_;
-		}
-		return out.str();
-	}
+  // Write rational as <[numerator]/[denominator]> or <[numerator]> if simplifiable
+  std::string as_string() const {
+    std::ostringstream out;
+    out << "<";
+    if (denominator_ == T{1}) {
+      out << numerator_;
+    } else {
+      out << numerator_ << "/" << denominator_;
+    }
+    out << ">";
+    return out.str();
+  }
 
-	// Compound assignment operators
-	rational_t& operator+=(const rational_t& rhs) noexcept {
-		numerator_ = numerator_ * rhs.denominator_ + rhs.numerator_ * denominator_;
-		denominator_ = denominator_ * rhs.denominator_;
-		normalize();
-		return *this;
-	}
-	rational_t& operator-=(const rational_t& rhs) noexcept {
-		numerator_ = numerator_ * rhs.denominator_ - rhs.numerator_ * denominator_;
-		denominator_ = denominator_ * rhs.denominator_;
-		normalize();
-		return *this;
-	}
-	rational_t& operator*=(const rational_t& rhs) noexcept {
-		numerator_ = numerator_ * rhs.numerator_;
-		denominator_ = denominator_ * rhs.denominator_;
-		normalize();
-		return *this;
-	}
-	rational_t& operator/=(const rational_t& rhs) {
-		if (rhs.is_zero()) {
-			throw division_by_zero_error("division by zero rational number");
-		}
-		numerator_ = numerator_ * rhs.denominator_;
-		denominator_ = denominator_ * rhs.numerator_;
-		normalize();
-		return *this;
-	}
+  // Compound assignment operators
+  rational_t &operator+=(const rational_t &rhs) noexcept {
+    numerator_ = numerator_ * rhs.denominator_ + rhs.numerator_ * denominator_;
+    denominator_ = denominator_ * rhs.denominator_;
+    normalize();
+    return *this;
+  }
+  rational_t &operator-=(const rational_t &rhs) noexcept {
+    numerator_ = numerator_ * rhs.denominator_ - rhs.numerator_ * denominator_;
+    denominator_ = denominator_ * rhs.denominator_;
+    normalize();
+    return *this;
+  }
+  rational_t &operator*=(const rational_t &rhs) noexcept {
+    numerator_ = numerator_ * rhs.numerator_;
+    denominator_ = denominator_ * rhs.denominator_;
+    normalize();
+    return *this;
+  }
+  rational_t &operator/=(const rational_t &rhs) {
+    if (rhs.is_zero()) {
+      throw division_by_zero_error("division by zero rational number");
+    }
+    numerator_ = numerator_ * rhs.denominator_;
+    denominator_ = denominator_ * rhs.numerator_;
+    normalize();
+    return *this;
+  }
 
-	// Binary arithmetic operators (delegating to compound) – Barton–Nackman as friends
-	friend rational_t operator+(rational_t lhs, const rational_t& rhs) noexcept { lhs += rhs; return lhs; }
-	friend rational_t operator-(rational_t lhs, const rational_t& rhs) noexcept { lhs -= rhs; return lhs; }
-	friend rational_t operator*(rational_t lhs, const rational_t& rhs) noexcept { lhs *= rhs; return lhs; }
-	friend rational_t operator/(rational_t lhs, const rational_t& rhs) { lhs /= rhs; return lhs; }
+  // Binary arithmetic operators (delegating to compound), defined as friends
+  friend rational_t operator+(rational_t lhs, const rational_t &rhs) noexcept {
+    lhs += rhs;
+    return lhs;
+  }
+  friend rational_t operator-(rational_t lhs, const rational_t &rhs) noexcept {
+    lhs -= rhs;
+    return lhs;
+  }
+  friend rational_t operator*(rational_t lhs, const rational_t &rhs) noexcept {
+    lhs *= rhs;
+    return lhs;
+  }
+  friend rational_t operator/(rational_t lhs, const rational_t &rhs) {
+    lhs /= rhs;
+    return lhs;
+  }
 
-	// Comparisons
-	friend bool operator==(const rational_t& a, const rational_t& b) noexcept {
-		return ops::equals(a.numerator_, b.numerator_) && ops::equals(a.denominator_, b.denominator_);
-	}
-	friend bool operator!=(const rational_t& a, const rational_t& b) noexcept { return !(a == b); }
-	friend bool operator<(const rational_t& a, const rational_t& b) noexcept {
-		return (a.numerator_ * b.denominator_) < (b.numerator_ * a.denominator_);
-	}
-	friend bool operator>(const rational_t& a, const rational_t& b) noexcept { return b < a; }
-	friend bool operator<=(const rational_t& a, const rational_t& b) noexcept { return !(b < a); }
-	friend bool operator>=(const rational_t& a, const rational_t& b) noexcept { return !(a < b); }
+  // Comparisons
+  friend bool operator==(const rational_t &a, const rational_t &b) noexcept {
+    return ops::equals(a.numerator_, b.numerator_) &&
+           ops::equals(a.denominator_, b.denominator_);
+  }
+  friend bool operator!=(const rational_t &a, const rational_t &b) noexcept {
+    return !(a == b);
+  }
+  friend bool operator<(const rational_t &a, const rational_t &b) noexcept {
+    return (a.numerator_ * b.denominator_) < (b.numerator_ * a.denominator_);
+  }
+  friend bool operator>(const rational_t &a, const rational_t &b) noexcept {
+    return b < a;
+  }
+  friend bool operator<=(const rational_t &a, const rational_t &b) noexcept {
+    return !(b < a);
+  }
+  friend bool operator>=(const rational_t &a, const rational_t &b) noexcept {
+    return !(a < b);
+  }
 
-	// Stream operators
-	friend std::ostream& operator<<(std::ostream& os, const rational_t& r) {
-		os << '<' << r.as_string() << '>';
-		return os;
-	}
-	friend std::istream& operator>>(std::istream& is, rational_t& r) {
-		T num{};
-		T den{1};
-		is >> std::ws;
-		if (!(is >> num)) {
-			return is;
-		}
-		if (is.peek() == '/') {
-			is.get();
-			if (!(is >> den)) {
-				is.setstate(std::ios::failbit);
-				return is;
-			}
-		}
-		if (ops::is_zero(den)) {
-			is.setstate(std::ios::failbit);
-			throw invalid_rational_error("attempt to construct rational with zero denominator from stream");
-		}
-		r = rational_t(num, den);
-		return is;
-	}
+  // Stream operators
+  friend std::ostream &operator<<(std::ostream &os, const rational_t &r) {
+    os << r.as_string();
+    return os;
+  }
+  friend std::istream &operator>>(std::istream &is, rational_t &r) {
+    // Accepted formats: "<n>" or "<n/d>"
+    T num{};
+    T den{1};
+    
+    is >> std::ws;
+    
+    // Expect opening angle bracket
+    if (is.peek() != '<') {
+      is.setstate(std::ios::failbit);
+      return is;
+    }
+    is.get();
+    
+    // Read numerator
+    if (!(is >> num)) {
+      is.setstate(std::ios::failbit);
+      return is;
+    }
+    
+    // Check for optional denominator
+    if (is.peek() == '/') {
+      is.get();
+      if (!(is >> den)) {
+        is.setstate(std::ios::failbit);
+        return is;
+      }
+    }
+    
+    // Expect closing angle bracket
+    is >> std::ws;
+    if (is.peek() != '>') {
+      is.setstate(std::ios::failbit);
+      return is;
+    }
+    is.get();
+    
+    // Validate denominator
+    if (ops::is_zero(den)) {
+      is.setstate(std::ios::failbit);
+      throw invalid_rational_error(
+          "attempt to construct rational with zero denominator from stream");
+    }
+    
+    r = rational_t(num, den);
+    return is;
+  }
 
 private:
-	void normalize() noexcept {
-		// move sign to numerator if denominator is negative
-		if (ops::is_negative(denominator_)) {
-			numerator_ = ops::negate(numerator_);
-			denominator_ = ops::negate(denominator_);
-		}
-		// Canonical zero: 0/x -> 0/1
-		if (ops::is_zero(numerator_)) {
-			denominator_ = T{1};
-			return;
-		}
-		// Reduce if we know how
-		if constexpr (std::is_same_v<T, int>) {
-			auto g = ops::gcd(numerator_, denominator_);
-			if (g != 0 && g != 1) {
-				numerator_ /= g;
-				denominator_ /= g;
-			}
-		} else if constexpr (is_matrix<T>::value) {
-			auto g = ops::gcd(numerator_, denominator_);
-			if (!ops::equals(g, T{1})) {
-				numerator_ = numerator_ / g;
-				denominator_ = denominator_ / g;
-			}
-		}
-	}
+  // Keep invariant: denominator non-negative, zero normalized, and reduce if
+  // possible
+  void normalize() noexcept {
+    // Move sign to numerator if denominator is negative
+    if (denominator_ < T{0}) {
+      numerator_ = -numerator_;
+      denominator_ = -denominator_;
+    }
+    // Canonical zero: 0/x -> 0/1
+    if (is_zero()) {
+      denominator_ = T{1};
+      return;
+    }
 
-	value_type numerator_;
-	value_type denominator_;
+    // Reduce when a gcd is defined for T, a > 0 guarandeed
+    T a = is_positive() ? numerator_ : -numerator_;
+    T b = denominator_;
+    while (b > T{0}) {
+        T c = a;
+        a = b % c;
+        b = c;
+    }
+    numerator_ = numerator_ / a;
+    denominator_ = denominator_ / a;
+  }
+
+  value_type numerator_;
+  value_type denominator_;
 };
 
 // Convenience overloads for mixing with int on the LHS for rational_t<int>
-inline rational_t<int> operator+(int lhs, const rational_t<int>& rhs) noexcept { return rational_t<int>(lhs) + rhs; }
-inline rational_t<int> operator-(int lhs, const rational_t<int>& rhs) noexcept { return rational_t<int>(lhs) - rhs; }
-inline rational_t<int> operator*(int lhs, const rational_t<int>& rhs) noexcept { return rational_t<int>(lhs) * rhs; }
-inline rational_t<int> operator/(int lhs, const rational_t<int>& rhs) { return rational_t<int>(lhs) / rhs; }
-
-
+inline rational_t<int> operator+(int lhs, const rational_t<int> &rhs) noexcept {
+  return rational_t<int>(lhs) + rhs;
+}
+inline rational_t<int> operator-(int lhs, const rational_t<int> &rhs) noexcept {
+  return rational_t<int>(lhs) - rhs;
+}
+inline rational_t<int> operator*(int lhs, const rational_t<int> &rhs) noexcept {
+  return rational_t<int>(lhs) * rhs;
+}
+inline rational_t<int> operator/(int lhs, const rational_t<int> &rhs) {
+  return rational_t<int>(lhs) / rhs;
+}
